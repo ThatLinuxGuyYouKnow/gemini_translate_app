4302753807d4db242758f86abbc2fd6acada5a77
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-04-16 21:05:55 +0100

    chore: improved method for getting translation

---
+++ lib/logic/getTranslation.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<Map<String, dynamic>> getTranslation({
+  required String originalLanguage,
+  required String targetLanguage,
+  required String targetText,
+}) async {
+  try {
+    String apiKey = getApiKey();
+
+    final url =
+        Uri.parse('https://gemini-translate-dzxg.onrender.com/translate');
+
+    final Map<String, String> requestBody = {
+      "original_language": originalLanguage,
+      "target_language": targetLanguage,
+      "text": targetText,
+      "api_key": apiKey,
+    };
+
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(requestBody),
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseData = jsonDecode(response.body);
+      return responseData;
+    } else {
+      throw Exception('Failed to translate text: ${response.body}');
+    }
+  } catch (e) {
+    throw Exception('Error during translation: $e');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return CupertinoApp(
       debugShowCheckedModeBanner: false,
-      title: 'Turbo Shark',
+      title: 'Gemini Translate',
       theme: const CupertinoThemeData(
         brightness: Brightness.dark, // Set the app to dark mode
         primaryColor: CupertinoColors.systemOrange,
